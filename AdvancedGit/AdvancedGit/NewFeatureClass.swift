8a568cf411a9a3d5fb0bcd3726a2966a1991b177
Daniel No <dno@averoinc.com>
2016-12-08 15:13:38 -0500

    new features!

---
+++ AdvancedGit/AdvancedGit/NewFeatureClass.swift
@@ -0,0 +1,21 @@
+//
+//  NewFeatureClass.swift
+//  AdvancedGit
+//
+//  Created by Daniel No on 12/8/16.
+//  Copyright © 2016 Daniel No. All rights reserved.
+//
+
+import UIKit
+
+class NewFeatureClass: NSObject {
+    
+    
+    
+    
+    
+    func testFeature1() -> Void {
+        print("feature!")
+    }
+
+}
