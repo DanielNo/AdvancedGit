8f190da36635df7da4c985841366b6e1a3bfb5b0
Daniel No <dno@averoinc.com>
2016-12-08 14:43:50 -0500

    conflict from dev

--- AdvancedGit/AdvancedGit/AppDelegate.swift
+++ AdvancedGit/AdvancedGit/AppDelegate.swift
@@ -13,7 +13,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
+    //conflict from development
     //commit 1
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
