efc77f06feb44160ff365925dd7d5ea19ea301b9
Daniel No <dno@averoinc.com>
2016-12-09 15:32:09 -0500

    development work 1

---
+++ AdvancedGit/AdvancedGit/DevelopmentTeamDoingStuff.swift
@@ -0,0 +1,22 @@
+//
+//  DevelopmentTeamDoingStuff.swift
+//  AdvancedGit
+//
+//  Created by Daniel No on 12/9/16.
+//  Copyright © 2016 Daniel No. All rights reserved.
+//
+
+import UIKit
+
+class DevelopmentTeamDoingStuff: NSObject {
+    
+    func devDoinStuff() -> Void {
+        print("feature2!")
+    }
+
+    
+    
+    
+    
+    
+}
