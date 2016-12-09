88e8973e32de32d3c0a98c4ecb5d3b7b82cd2c66
Daniel No <dno@averoinc.com>
2016-12-09 16:38:23 -0500

    adding to this branch only!

---
+++ AdvancedGit/AdvancedGit/NewBranchTreeFile.swift
@@ -0,0 +1,16 @@
+//
+//  NewBranchTreeFile.swift
+//  AdvancedGit
+//
+//  Created by Daniel No on 12/9/16.
+//  Copyright © 2016 Daniel No. All rights reserved.
+//
+
+import UIKit
+
+class NewBranchTreeFile: NSObject {
+    
+    
+    //I only have this!
+
+}
