5a5bf29888254485aa94b2a8c04503a42cfd0bf8
Daniel No <dno@averoinc.com>
2016-12-09 10:36:40 -0500

    add another feature

---
+++ AdvancedGit/AdvancedGit/AnotherFeatureClass.swift
@@ -0,0 +1,20 @@
+//
+//  AnotherFeatureClass.swift
+//  AdvancedGit
+//
+//  Created by Daniel No on 12/9/16.
+//  Copyright © 2016 Daniel No. All rights reserved.
+//
+
+import UIKit
+
+class AnotherFeatureClass: NSObject {
+    
+    
+    
+    
+    func anotherFeature1() -> Void {
+        
+    }
+
+}
