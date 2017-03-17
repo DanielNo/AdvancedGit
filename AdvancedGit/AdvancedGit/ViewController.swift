4db93c76c53e72c2af6cbd87ab94d07ad0974da4
Daniel No <dno@averoinc.com>
2017-03-17 14:57:46 -0400

    COOL FEATURE - GIT FLOW

--- AdvancedGit/AdvancedGit/ViewController.swift
+++ AdvancedGit/AdvancedGit/ViewController.swift
@@ -20,6 +20,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func checkOutThisCoolGitFlowFeature(){
+        print("this feature is so cool!!")
+    
+    }
 
 }
 
